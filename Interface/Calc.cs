126d48ad1f8b1f1a508013179d869088291da58d
Николай Стригачев <nikolai7832@yandex.ru>
2022-04-14 19:51:42 +0300

    Добавьте файлы проекта.

---
+++ Interface/Calc.cs
@@ -0,0 +1,43 @@
+﻿namespace Interface
+{
+    public class Calculator : ICalc
+    {
+        int a;
+        int b;
+        string str;
+        Logger Logger { get; }
+        public void Calc(Logger Logger)
+        {
+            Logger.Event("Введите значение");
+            str = Console.ReadLine();
+            try
+            {
+                Int32.Parse(str);
+            }
+            catch (Exception)
+            {
+                Logger.Error("Возниклала ошибка");
+                Calc(Logger);
+            }
+
+            a = Int32.Parse(str);
+            Logger.Event($"Первое значение:{a}");
+
+            Logger.Event("Введите значение");
+            str = Console.ReadLine();
+            try
+            {
+                Int32.Parse(str);
+            }
+            catch (Exception)
+            {
+                Logger.Error("Возниклала ошибка");
+                Calc(Logger);
+            }
+
+            b = Int32.Parse(str);
+            Logger.Event($"Второе значение:{b}");
+            Logger.Event($"Сумма чисел:{a + b}");
+        }
+    }
+}
---
+++ Interface/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Interface
+{
+    public class Programm
+    {
+        static Logger Logger { get; set; }
+        static void Main()
+        {
+            Logger = new Logger();
+            Calculator calculator = new Calculator();
+            calculator.Calc(Logger);
+
+        }
+    }
+    public interface ILogger
+    {
+        static public void Error(string message)
+        { }
+        static public void Event(string message)
+        { }
+    }
+    public class Logger : ILogger
+    {
+        static public void Error(string message)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+        }
+
+        static public void Event(string message)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine(message);
+        }
+    }
+    public interface ICalc
+    {
+        public void Calc(Logger Logger);
+    }
+
+}
